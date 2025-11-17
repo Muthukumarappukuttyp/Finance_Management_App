ada47bba4db583c5642ffa678a1da663eb565768
Muthu Kumar <muthukumar26022005@gmail.com>
2025-11-17 22:08:26 +0530

    [Muthukumar] push updated route formatting json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/Finance_Management_App/"
+  plugins: [react(), tailwindcss()]
+
 })
